749d99377aec77388a65588edf9b1947c006555d
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-11-07 08:12:15 -0800

    Add files via upload

---
+++ GenericDataType.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class GenericDataType{
+  public static void main(String[] args) {
+    List<String> l = new ArrayList<String>();
+    l.add("Hello");
+    System.out.println(l.get(0));
+  }
+}
