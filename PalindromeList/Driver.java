c4840593e8105bd3077e5404a08e8c1369fbebfe
Yash Khare <yashsja@gmail.com>
2019-07-10 14:21:56 +0530

    Create Driver.java

---
+++ PalindromeList/Driver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Driver {
+	public static void main(String[] args) {
+		Palin s1 = new Palin();
+		String s;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter word to be checked");
+		s = scan.nextLine();
+		for (int i = 0; i < s.length(); i++) {
+			s1.push(s.charAt(i));
+		}
+		for (int i = 0; i < s.length(); i++) {
+			s1.reverse();
+		}
+		if (s.equals(s1.s)) {
+			System.out.println("It is palindrome");
+		} else {
+			System.out.println("It ain't palindrome");
+		}
+
+	}
+}
