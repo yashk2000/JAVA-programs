3d9df5dd44a5effb9ebfd977ca4edb9f73068ef2
Yash Khare <yashsja@gmail.com>
2019-08-19 09:54:59 +0530

    Update Driver.java

--- File IO/Driver.java
+++ File IO/Driver.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Driver {
 	public static void main(String[]args) throws IOException, FileNotFoundException {
 		
-		File file = new File("/home/examuser/input.txt");
+		File file = new File("input.txt"); //correct path to input.txt
 		Scanner sc = new Scanner(file);
 		int p1 = sc.nextInt();
 		int p2 = sc.nextInt();
@@ -17,7 +17,7 @@ public class Driver {
 		int d = (p1 - p3) * (p1 - p3);
 		int d2 = (p2 - p4)  * (p2 - p4);
 		double ans = Math.sqrt(d + d2);
-		File file2 = new File("/home/examuser/output.txt");
+		File file2 = new File("output.txt"); // correct path to output.txt
 		FileWriter fw = new FileWriter(file2);
 		String s = Double.toString(ans);
 		fw.write(s);
