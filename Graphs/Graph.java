27e0661ce63fdd95342c67aca57f6769f6b982f7
Yash Khare <yashsja@gmail.com>
2019-09-18 14:09:49 +0530

    Create Graph.java

---
+++ Graphs/Graph.java
@@ -0,0 +1,32 @@
+
+public class Graph {
+	GraphNode[] node;
+	int size;
+	
+	public Graph(int n) {
+		size = n;
+		node = new GraphNode[size];
+		for(int i = 0; i < size; ++i) {
+			node[i] = new GraphNode(i);
+		}
+	}
+	
+	public void addEdge(int from, int to) {
+		node[from].adjList.add(node[to]);
+	}
+	
+	public void print() {
+		for(int i = 0; i < size; ++i) {
+			System.out.print(i + ": ");
+			node[i].print();
+			System.out.println();
+		}
+	}
+	
+	public void dfs() {
+		for (int i=0; i<size; i++) {
+		if ( !node[i].visited )
+		node[i].dfs();
+		}
+	}
+}
