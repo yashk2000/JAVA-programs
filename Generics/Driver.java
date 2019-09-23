ea6be1f1a3ceb426ac3aac3eaa755bd77d305f53
Yash Khare <yashsja@gmail.com>
2019-09-23 10:25:06 +0530

    Create Driver.java

---
+++ Generics/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+	public static void main(String[]args) {
+		printAnyArray.printArray(new Integer[] {1, 2, 3, 4, 5});
+		printAnyArray.printArray(new Double[] {4.5, 6.7, 8.9, 1.2});
+		printAnyArray.printArray(new String[] {"hh", "sgg", "srtRT"});
+	}
+}
