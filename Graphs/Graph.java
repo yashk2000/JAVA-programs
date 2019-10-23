37a62cb642e7c3378fbf3624ef072f8a332e6bf4
Yash Khare <yashsja@gmail.com>
2019-10-23 05:30:25 +0530

    Updated DFS, BFS, printOrder

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -1,45 +1,66 @@
 import java.util.*;
-public class Graph {
-	GraphNode[] node;
-	int size;
-	
-	public Graph(int n) {
-		size = n;
-		node = new GraphNode[size];
-		for(int i = 0; i < size; ++i) {
-			node[i] = new GraphNode(i);
-		}
-	}
-	
-	public void addEdge(int from, int to) {
-		node[from].adjList.add(node[to]);
-	}
-	
-	public void print() {
-		for(int i = 0; i < size; ++i) {
-			System.out.print(i + ": ");
-			node[i].print();
-			System.out.println();
-		}
-	}
-	
-	public void dfs() {
-		for (int i=0; i<size; i++) {
-		if ( !node[i].visited )
-		node[i].dfs();
-		}
-	}
-	
-	public void bfs() {
-		for(int i = 0;i < size; ++i) {
-			node[i].visited = false;
-		}
-		
-		Queue<GraphNode> q = new LinkedList<GraphNode>();
-		for(int i = 0; i < size; ++i) {
-			if(!node[i].visited)
-			node[i].bfs(q);
-		}
-		System.out.println();
-	}
+class Graph {
+    GraphNode[]node ;
+    int size;
+    
+    public Graph(int n) {
+        size = n;
+        node = new GraphNode[n];
+        for(int i = 0; i < n; ++i){
+            node[i] = new GraphNode(i);
+        }
+    }
+    
+    public void addEdge(int from, int to) {
+        node[from].adjList.add(node[to]);
+    }
+    
+    public void print() {
+        for(int i = 0; i < size; ++i){
+            System.out.print(i + ": ");
+            node[i].print();
+            System.out.println();
+        }
+    }
+    
+    public void dfs() {
+        reset();
+        int visitedCount = 0;
+        for(int i = 0; i < size; ++i) {
+            if(!node[i].visited) {
+                visitedCount = node[i].dfs(++visitedCount);
+            }
+        System.out.println();
+        }
+    }
+    
+    public void printOrder() {
+        for(int i = 0; i < size; ++i) {
+            System.out.print(i + "(" + node[i].preorder + ", " + node[i].postorder + ") ");
+        }
+        System.out.println();
+    }
+    
+    public void reset(){
+        for(int i = 0; i < size; ++i){
+            node[i].postorder = 0;
+            node[i].preorder = 0;
+            node[i].visited = false;
+        }
+    }
+    
+    public void bfs() {
+        ArrayDeque<GraphNode> q = new ArrayDeque<GraphNode>();
+        for(int i = 0; i < size; ++i){
+            if(!node[i].visited) {
+                q.add(node[i);
+                node[i].visited = true;
+                node[i].bfs(q);
+            }
+        }
+    }
 }
+    
+    
+    
+    
